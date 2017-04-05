0923a67c7f08df58ad1116579fbe824ea99eea0c
Anger <Anger>
2017-04-05 13:46:37 +0200

    second method in progress

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
+#include <QPixmap>
 #include <QImage>
 #include <QPainter>
 #include <QColor>
@@ -16,6 +17,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     pixmap = NULL;
     filePath = QString("");
+
+    alignParam = this->size();
+    iniPos = alignParam.width()/2 - 300/2;
+    cubeSize = 10;
+    topOffset = 50;
 }
 
 MainWindow::~MainWindow()
@@ -28,69 +34,89 @@ void MainWindow::paintEvent(QPaintEvent*e)
 {
     //we create a Drawing context and attach it to the calling object, namely the main window
     QPainter painter(this);
-    QSize alignParam = this->size();
-    int iniPos = alignParam.width()/2 - 300/2;
-    int cubeSize = 10;
-    int topOffset = 50;
 
-    if(function == 1){
-        if(pixmap)
-        {
-            //we create a rendering square (200x400 pixels size, located at point 300, 50
-            QRect R( iniPos, topOffset, 400, 400 );
-            painter.scale(1,1);
-            painter.drawPixmap(R, *pixmap); //once again, the image will be stretched... How could you do to overcome this issue??
-        }
-    }else if(function ==2){
+    if(pixmap) {
+        //we create a rendering square (200x400 pixels size, located at point 300, 50
+        QRect R( iniPos, topOffset, 400, 400 );
+        painter.scale(1,1);
+        painter.drawPixmap(R, *pixmap); //once again, the image will be stretched... How could you do to overcome this issue??
+    }
 
-        for(int i = 0; i < img->width(); i+=cubeSize){
-            for(int j = 0; j < img->height(); j+=cubeSize){
+}
 
-//                QVector<QRgb> colorVal;
-//                QList<int> pixelVal;
-//                QRgb temp;
 
-                QList<int> redList, greenList, blueList, alphaList;
-                int meanRed, meanGreen, meanBlue, meanAlpha;
+void MainWindow::on_btnLoad_clicked()
+{
+    //same as drawing in Label: we first get the path to an image to be drawn
+    filePath = QFileDialog::getOpenFileName(
+                this,
+                "Open a file",
+                QString(),
+                "Images (*.png *.gif *.jpg *.jpeg)");
 
-                for(int k = i; k < i+cubeSize; k++){
-                    for(int l = j; l < j+cubeSize; l++){
-                        int r,g,b,a;
+    if (filePath.isEmpty()) return;
 
-//                        colorVal.append(img->pixel(k,l));
-                        QColor color(img->pixel(k,l));
-                        color.getRgb(&r, &g, &b, &a);
+    img = new QImage(filePath);
 
-                        redList.append(r);
-                        greenList.append(g);
-                        blueList.append(b);
-                        alphaList.append(a);
-                    }
+    updatePixmap();
+}
+
+void MainWindow::on_btnPixelize_clicked()
+{
+
+    for(int i = 0; i < img->width(); i+=cubeSize){
+        for(int j = 0; j < img->height(); j+=cubeSize){
+
+//          QVector<QRgb> colorVal;
+
+            QList<int> redList, greenList, blueList, alphaList;
+            int meanRed, meanGreen, meanBlue, meanAlpha;
+            int loopRestricterX, loopRestricterY;
+
+            i+cubeSize > img->width()
+                    ? loopRestricterX = i + (i+cubeSize - img->width())
+                    : loopRestricterX = i+cubeSize-1;
+
+            j+cubeSize > img->height()
+                    ? loopRestricterY = j + (j+cubeSize - img->height())
+                    : loopRestricterY = j+cubeSize-1;
+
+
+            for(int k = i; k <= loopRestricterX; k++){
+                for(int l = j; l <= loopRestricterY; l++){
+                    int r,g,b,a;
+
+//                  colorVal.append(img->pixel(k,l));
+                    QColor color(img->pixel(k,l));
+                    color.getRgb(&r, &g, &b, &a);
+
+                    redList.append(r);
+                    greenList.append(g);
+                    blueList.append(b);
+                    alphaList.append(a);
                 }
+            }
 
-                for(int m = 0; m < redList.length(); m++)
-                    meanRed += redList[m];
-                for(int n = 0; n < redList.length(); n++)
-                    meanGreen += greenList[n];
-                for(int o = 0; o < redList.length(); o++)
-                    meanBlue += blueList[o];
-                for(int p = 0; p < alphaList.length(); p++)
-                    meanAlpha += alphaList[p];
+            for(int m = 0; m < redList.length(); m++)
+                meanRed += redList[m];
+            for(int n = 0; n < redList.length(); n++)
+                meanGreen += greenList[n];
+            for(int o = 0; o < redList.length(); o++)
+                meanBlue += blueList[o];
+            for(int p = 0; p < alphaList.length(); p++)
+                meanAlpha += alphaList[p];
 
-                meanRed = meanRed/redList.length();
-                meanGreen = meanGreen/greenList.length();
-                meanBlue = meanBlue/blueList.length();
-                meanAlpha = meanAlpha/alphaList.length();
-                QRgb meanColor = qRgb(meanRed,meanGreen,meanBlue);
+            meanRed = meanRed/redList.length();
+            meanGreen = meanGreen/greenList.length();
+            meanBlue = meanBlue/blueList.length();
+            meanAlpha = meanAlpha/alphaList.length();
 
-//                QColor meanColor(meanRed,meanGreen,meanBlue,meanAlpha);
+            QRgb meanColor = qRgba(meanRed,meanGreen,meanBlue,meanAlpha);
 
-                for(int q = i; q < i+cubeSize; q++){
-                    for(int r = j; r < j+cubeSize; r++)
-                        img->setPixel(q,r,meanColor);
-
-
-                }
+            for(int q = i; q <= loopRestricterX; q++){
+                for(int r = j; r <= loopRestricterY; r++)
+                    img->setPixel(q,r,meanColor);
+            }
 
 //                for(int m = 0; m < colorVal.length(); m++){
 //                    QColor color(colorVal[m]);
@@ -107,13 +133,6 @@ void MainWindow::paintEvent(QPaintEvent*e)
 //                QRgb mean = temp/colorVal.size();
 //                int mean = temp/pixelVal.size();
 
-//                qDebug() << colorVal.length();
-//                qDebug() << colorVal[0];
-//                qDebug() << colorVal[1];
-//                qDebug() << colorVal[2];
-//                qDebug() << colorVal[3];
-//                qDebug() << mean;
-
 //                QColor color(mean);
 //                Qt::BrushStyle style = Qt::SolidPattern;
 //                QBrush brush(color, style);
@@ -124,44 +143,23 @@ void MainWindow::paintEvent(QPaintEvent*e)
 //                painter.drawRect(rect);
 
 
-            }
         }
     }
 
-}
-
-
-void MainWindow::on_btnLoad_clicked()
-{
-    function = 1;
-    //same as drawing in Label: we first get the path to an image to be drawn
-    filePath = QFileDialog::getOpenFileName(
-                this,
-                "Open a file",
-                QString(),
-                "Images (*.png *.gif *.jpg *.jpeg)");
-
-    if (filePath.isEmpty()) return;
-
-    if (pixmap) delete pixmap;
-
-    //we create a pixmap
-    //    QImage *img = new QImage();
-    img = new QImage(filePath);
-    pixmap = new QPixmap(filePath);
-
-    cout <<pixmap<<endl;
-    //we create a rectangle in which we are going to draw the pixmap
-    update();
-    cout <<"done"<<endl;
-}
-
-void MainWindow::on_btnPixelize_clicked()
-{
-    function = 2;
+    updatePixmap();
 }
 
 void MainWindow::on_btnSave_clicked()
 {
 
+}
+
+void MainWindow::updatePixmap(){
+    if (pixmap) delete pixmap;
+
+    //    QImage *img = new QImage();
+    pixmap = new QPixmap::fromImage(img);
+
+    update();
+    cout <<"done updating image"<<endl;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -23,12 +23,18 @@ private slots:
 
     void on_btnPixelize_clicked();
 
+    void updatePixmap();
+
 private:
     Ui::MainWindow *ui;
     QImage *img;
     QString filePath; // path to the selected image
     QPixmap *pixmap;  //pointer to pixamp
     int function;
+    QSize alignParam;
+    int iniPos;
+    int cubeSize;
+    int topOffset;
 };
 
 #endif // MAINWINDOW_H
